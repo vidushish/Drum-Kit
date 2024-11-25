18a787e9da9731cfd4d1e1f1e3daced1c632bde9
vidushish <vidushi.sh12@gmail.com>
2024-11-26 00:08:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Drum-Kit
+ "Interactive drum kit created using front-end technologies, enabling users to play drum sounds through keyboard or mouse input."
---
+++ js_drumkit.js
@@ -0,0 +1,50 @@
+//button press
+for(var i=0;i<document.querySelectorAll(".drum").length;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        var buttonInnerHTML=this.innerHTML;
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+    });
+}
+
+//key press
+document.addEventListener("keydown",function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+    
+});
+function makeSound(key){
+    switch (key){
+        case "w":
+            new Audio("./sounds/tom-1.mp3").play();
+            break;
+        case "a":
+            new Audio("./sounds/tom-2.mp3").play();
+            break;
+        case "s":
+            new Audio("./sounds/tom-3.mp3").play();
+            break;
+        case "d":
+            new Audio("./sounds/tom-4.mp3").play();
+            break;
+        case "j":
+            new Audio("./sounds/snare.mp3").play();
+            break;
+        case "k":
+            new Audio("./sounds/crash.mp3").play();
+            break;
+        case "l":
+            new Audio("./sounds/kick-bass.mp3").play();
+            break;
+        default:
+            break;
+    }
+}
+
+function buttonAnimation(currentKey){
+    var activateButton=document.querySelector("."+currentKey);
+    activateButton.classList.add("Pressed");
+    setTimeout(function (){
+        activateButton.classList.remove("Pressed");
+    },100);
+}
